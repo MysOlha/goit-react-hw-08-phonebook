fee2d64c0b42fb5e7b32628c0c0cb4908ac6c8da
MysOlha <misarosh70@gmail.com>
2022-12-02 19:31:30 +0200

    start

---
+++ src/redux/actions.jsx
@@ -0,0 +1,7 @@
+// import { createAction } from '@reduxjs/toolkit';
+
+// export const addContact = createAction('contacts/AddContact');
+
+// export const deleteContact = createAction('contacts/deleteContact');
+
+// export const filterContact = createAction('contacts/filterContact');
